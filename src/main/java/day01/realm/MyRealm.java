458728c0ba4406276ab89717dfcacf5b774965c5
HWOLFeng <2295566872@qq.com>
2017-10-24 11:41:32 +0800

    Initial commit

---
+++ src/main/java/day01/realm/MyRealm.java
@@ -0,0 +1,39 @@
+package day01.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @author HWOLF
+ * @version create in：17-10-22 下午9:03
+ * @descrption
+ */
+public class MyRealm implements Realm{
+    public static final String NAME = "hwf";
+    public static final String PASS = "hwf123";
+    @Override
+    public String getName() {
+        return "myRealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();
+        String password = new String((char[])authenticationToken.getCredentials());
+        String testPassword = (String) authenticationToken.getCredentials();
+        if (!NAME.equals(username)){
+            throw new UnknownAccountException();
+        }
+        if (!PASS.equals(password)){
+            throw new IncorrectCredentialsException();
+        }
+        System.out.println(password);
+        System.out.println(testPassword);
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
